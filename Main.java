2db1aa28787605d4f521a9c480a16dd844c7fb5f
nkhtmmdv <mamedoffnikh@gmail.com>
2025-03-15 22:54:56 +0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,113 +1,77 @@
-import java.util.Arrays;
+import java.util.*;
 
-class Pet {
-    String species;
-    String nickname;
-    int age;
-    int trickLevel;
-    String[] habits;
+class Student implements Comparable<Student> {
+    int id;
+    String name;
+    String surname;
+    double grade;
+    String gender;
+    Date birthdate;
 
-    public Pet() {}
-
-    public Pet(String species, String nickname) {
-        this.species = species;
-        this.nickname = nickname;
+    public Student(int id, String name, String surname, double grade, String gender, Date birthdate) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.grade = grade;
+        this.gender = gender;
+        this.birthdate = birthdate;
     }
 
-    public Pet(String species, String nickname, int age, int trickLevel, String[] habits) {
-        this.species = species;
-        this.nickname = nickname;
-        this.age = age;
-        this.trickLevel = trickLevel;
-        this.habits = habits;
-    }
-
-    public void eat() {
-        System.out.println("I am eating");
-    }
-
-    public void respond() {
-        System.out.println("Hello, owner. I am " + nickname + ". I miss you!");
-    }
-
-    public void foul() {
-        System.out.println("I need to cover it up");
+    @Override
+    public int compareTo(Student other) {
+        return Integer.compare(this.id, other.id);
     }
 
     @Override
     public String toString() {
-        return species + "{nickname='" + nickname + "', age=" + age + ", trickLevel=" + trickLevel + ", habits=" + Arrays.toString(habits) + "}";
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", grade=" + grade +
+                ", gender='" + gender + '\'' +
+                ", birthdate=" + birthdate +
+                '}';
     }
 }
 
-class Human {
-    String name;
-    String surname;
-    int year;
-    int iq;
-    Pet pet;
-    Human mother;
-    Human father;
-    String[][] schedule;
-
-    public Human() {}
-
-    public Human(String name, String surname, int year) {
-        this.name = name;
-        this.surname = surname;
-        this.year = year;
-    }
-
-    public Human(String name, String surname, int year, Human father, Human mother) {
-        this.name = name;
-        this.surname = surname;
-        this.year = year;
-        this.father = father;
-        this.mother = mother;
-    }
-
-    public Human(String name, String surname, int year, int iq, Pet pet, Human mother, Human father, String[][] schedule) {
-        this.name = name;
-        this.surname = surname;
-        this.year = year;
-        this.iq = iq;
-        this.pet = pet;
-        this.mother = mother;
-        this.father = father;
-        this.schedule = schedule;
-    }
-
-    public void greetPet() {
-        System.out.println("Hello, " + pet.nickname);
-    }
-
-    public void describePet() {
-        String slyness = (pet.trickLevel > 50) ? "very sly" : "almost not sly";
-        System.out.println("I have an " + pet.species + " is " + pet.age + " years old, he is " + slyness);
-    }
-
+class GradeComparator implements Comparator<Student> {
     @Override
-    public String toString() {
-        return "Human{name='" + name + "', surname='" + surname + "', year=" + year + ", iq=" + iq +
-                ", mother=" + (mother != null ? mother.name + " " + mother.surname : "null") +
-                ", father=" + (father != null ? father.name + " " + father.surname : "null") +
-                ", pet=" + pet + "}";
+    public int compare(Student s1, Student s2) {
+        int gradeCompare = Double.compare(s2.grade, s1.grade);
+        if (gradeCompare != 0) return gradeCompare;
+        int birthdateCompare = s1.birthdate.compareTo(s2.birthdate);
+        if (birthdateCompare != 0) return birthdateCompare;
+        return s1.gender.compareTo(s2.gender);
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        Pet pet = new Pet("dog", "Rock", 5, 75, new String[]{"eat", "drink", "sleep"});
-        Human mother = new Human("Janet", "aabb", 1950);
-        Human father = new Human("Vitya", "aabb", 1945);
-        Human child = new Human("Misha", "aabb", 1977, 90, pet, mother, father, null);
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(5, "John", "Doe", 88.0, "Male", new Date(102, 3, 10)));
+        students.add(new Student(2, "Emily", "Clark", 91.5, "Female", new Date(99, 6, 25)));
+        students.add(new Student(4, "Michael", "Johnson", 88.0, "Male", new Date(102, 3, 10)));
+        students.add(new Student(1, "Sophia", "Miller", 95.0, "Female", new Date(98, 11, 5)));
+        students.add(new Student(3, "Daniel", "Smith", 91.5, "Male", new Date(99, 6, 25)));
 
-        System.out.println(child);
+        Collections.sort(students);
+        System.out.println("Sorted by ID:");
+        students.forEach(System.out::println);
 
-        child.greetPet();
-        child.describePet();
-        pet.eat();
-        pet.respond();
-        pet.foul();
+        students.sort(new GradeComparator());
+        System.out.println("\nSorted by Grade, Birthdate, and Gender:");
+        students.forEach(System.out::println);
+        
+        students.sort((s1, s2) -> {
+            int gradeCompare = Double.compare(s2.grade, s1.grade);
+            if (gradeCompare != 0) return gradeCompare;
+            int birthdateCompare = s1.birthdate.compareTo(s2.birthdate);
+            if (birthdateCompare != 0) return birthdateCompare;
+            return s1.gender.compareTo(s2.gender);
+        });
+
+        System.out.println("\nSorted using Lambda:");
+        students.forEach(System.out::println);
     }
 }
